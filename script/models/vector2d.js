042fbe3bb81b67677191d6bc2d28acf7fba35c42
FatherrRussia <vroman@cerebotani.it>
2024-05-06 17:28:00 +0200

    commit 4
    added vector2d.js file

---
+++ script/models/vector2d.js
@@ -0,0 +1,21 @@
+class Vector2D {
+    x;
+    y;
+
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    set(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    add(v) {
+        this.x += v.x;
+        this.y += v.y;
+    }
+}
+
+export default Vector2D;
